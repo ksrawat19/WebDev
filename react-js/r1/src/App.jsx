5ce1b4e42b71249f49d3f9b2be7df1cd1e414115
KsRawat <rawatks192@gmail.com>
2025-08-09 19:12:47 +0530

    react exercise 1

---
+++ react-js/r1/src/App.jsx
@@ -0,0 +1,19 @@
+import Greet from "./components/Greet";
+
+// function App(){
+//     return <h1>Good Job!</h1>
+// };
+
+
+// Or
+
+const App = () => {
+    return (
+        <div>
+            <h1>KsRawat</h1>
+            <Greet/>
+        </div>
+    );
+};
+
+export default App;
---
+++ react-js/r1/src/components/Greet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greet = ()=>{
+    return (
+        <h1>Namste!!</h1>
+    ) 
+}
+
+export default Greet;
