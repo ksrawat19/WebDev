ad67e58e4dbf4248153e70c00b2268b4e5c2dcda
KsRawat <rawatks192@gmail.com>
2025-08-11 21:03:26 +0530

    react exercise 18, advance keys

---
+++ react-js/r18/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Switcher from './Components/switcher'
+
+const App = () => {
+  return (
+    <div>
+        <Switcher />
+    </div>
+  )
+}
+
+export default App
