00722dd8b1f120bee4c8c564bbe3f9962c044037
KsRawat <rawatks192@gmail.com>
2025-09-14 10:30:24 +0530

    node-js intro

---
+++ node-js/2.REPL/index.js
@@ -0,0 +1,25 @@
+// Read-Eval-Print Loop (REPL)
+// Read: It reads the user’s input (a JavaScript expression).
+// Eval: It evaluates the input.
+// Print: It prints the result of the evaluation.
+// Loop: It goes back to waiting for the next input.
+
+// The REPL in Node.js stands for Read–Eval–Print–Loop, and it's basically an interactive playground for JavaScript. Think of it as a live coding console where you can type in code, run it instantly, and see the results—perfect for quick experiments, debugging, or just learning the ropes.
+
+// The REPL module provides a implementation that is avaliable both
+// as a standalone program or includible in other applications.
+
+// Basically it's a place where you can write/run JS code.
+// Just open your terminal and type: node
+
+// // Bonus Features
+// shift + enter to write multiple line code like functions
+// Underscore _ variable: Stores the result of the last expression.
+
+// .break (sometimes you get stuck, this gets you out)
+// .clear (Alias for .break)
+// .editor (Enter editor mode (Ctrl+D) to finish, (Ctrl+C) to cancel)
+// .exit (Exit the REPL)
+// .help (Print this help message)
+// .load (Loads JS from a file into the REPL session e.g .load .filepath)
+// .save (Save all evaluated commands in this REPL session to a file e.g .save /filepath)
